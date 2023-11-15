9e9f1e0b49ba97fc58606cd6f000f0be41775bc4
s-ly <SergeyLysov@outlook.com>
2023-11-15 07:21:39 +0300

    добавил отключение звука при сворачивании вкладки (версия опубликованна)

---
+++ Assets/Sound/SoundV2.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundV2 : MonoBehaviour
+{
+    void OnApplicationFocus(bool hasFocus) {
+        Silence(!hasFocus);
+    }
+
+    void OnApplicationPause(bool isPaused) {
+        Silence(isPaused);
+    }
+
+    private void Silence(bool silence) {
+        AudioListener.pause = silence;
+        // Or / And
+        AudioListener.volume = silence ? 0 : 1;
+    }
+}
